6a0c121338534cbaafab90e61410e92af5d22f5e
Nate Kinne <nathan.kinne@gmail.com>
2019-12-10 18:22:41 -0600

    config change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // 'https://arcane-anchorage-71509.herokuapp.com/api'
   // 'http://localhost:8000/api'
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://arcane-anchorage-71509.herokuapp.com/api',
   TOKEN_KEY: "techbook"
 };
